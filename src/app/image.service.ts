4413b043505d9ca80eee5e4f713c59101f3ee015
thdal <thdal@outlook.fr>
2020-12-30 15:15:26 +0100

    date parameters added

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -32,17 +32,19 @@ export class ImageService {
 
   getImages(p_text: string, p_gallerie: boolean, p_apiKey: string,
   p_dateMin: number, p_dateMax: number) {
-    console.log("-------->DATEMIN")
-    console.log(p_dateMin)
+    const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&';
+    var params = `api_key=${p_apiKey}&text=${p_text}&in_gallery=${p_gallerie}`;
     if(p_dateMin){
-      const date_min = `&min_upload_date=${p_dateMin}`
+      const date_min = `&min_upload_date=${p_dateMin}`;
+      params = params + date_min;
     }
     if(p_dateMax){
-      const date_max = `&max_upload_date=${p_dateMax}`
-    }
-    const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&';
-    const params = `api_key=${p_apiKey}&text=${p_text}&in_gallery=${p_gallerie}&format=json&nojsoncallback=1`;
+      const date_max = `&max_upload_date=${p_dateMax}`;
+      params = params + date_max;
 
+    }
+    const format = `&format=json&nojsoncallback=1`;
+    params = params + format;
     return this.http.get(url + params).pipe(map((res: FlickrOutput) => {
       const urlArr = [];
       const imagesObject = [];
